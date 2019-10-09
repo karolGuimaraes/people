f69159bd65f3dd54cfe21903fb04232273841100
karolinny <karolinny.silva@gmail.com>
2019-10-09 17:43:55 -0300

    1 commit

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+
+const Header = (props) => (
+    <View>
+        <Text>Header</Text>
+    </View>
+);
+
+export default Header;
